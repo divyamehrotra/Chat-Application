965f1eb59321dbcdd5b034b7939c23c18b4a3593
Divya Mehrotra <mehrotradivya54@gmail.com>
2024-05-10 16:22:32 +0530

    Basic UI

---
+++ client/src/Components/Sidebar.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import './Styles.css'
+
+function Sidebar() {
+  return (
+    <div className='sidebar-container'>
+      Sidebar
+    </div>
+  )
+}
+
+export default Sidebar
---
+++ client/src/Components/WorkArea.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import './Styles.css'
+
+function WorkArea() {
+  return (
+    <div className='workarea-container'>
+      WorkArea
+    </div>
+  )
+}
+
+export default WorkArea
